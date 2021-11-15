f2c83ff93a8a1c409237387a2a0301b3f14ec4f2
giusencito <66499088+giusencito@users.noreply.github.com>
2021-11-15 17:42:08 -0500

    springproject

---
+++ cartera/src/main/java/upc/edu/pe/cartera/CarteraApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.cartera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteraApplication.class, args);
+	}
+
+}
